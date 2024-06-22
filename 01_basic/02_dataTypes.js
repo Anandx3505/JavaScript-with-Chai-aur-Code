df9fd44c5f77e207894d3948691b0ffde69e10ed
Anand Chaudhary <221030123@juitsolan.in>
2024-06-22 13:09:17 +0530

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3) // we are using nodejs , not using the browser
+
+console.log(3+
+    3
+
+)// code readability should be high
+
+console.log("Anand")
+
+let name = "Anand"
+let age = 20
+let isLoggedIn = false
+let state;   // undefined
+
+//  number => -(2^53 - 1) to (2^53 - 1)
+//  big int
+//  boolean => true/false
+//  null => standalone value
+//  undefined => nothing was assigned thus not determined
+// symbol =>  unique
+
+//  object
+
+
+console.log(typeof "anand")
+console.log(typeof 123)
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
