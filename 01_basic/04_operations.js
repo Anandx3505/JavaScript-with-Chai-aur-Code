6dcc1d93f68c79cb735d022fa2cdcc2b2eafa70b
Anand Chaudhary <221030123@juitsolan.in>
2024-06-24 23:48:50 +0530

    Operators in JavaScript

---
+++ 01_basic/04_operations.js
@@ -0,0 +1,37 @@
+let value = 3
+let negValue = -value
+// console.log(negValue); //-3
+
+
+// Basic Operations
+// console.log(2+2)
+// console.log(2-1)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = "Anand"
+let str3 = str1+str2
+// console.log(str3); //helloAnand (no space included)
+
+// console.log("1"+2) //12
+// console.log(1+"2") //12
+// console.log("1"+2+2) //122 agar string first to baki bhi string consider karte hain
+// console.log(1+2+"2") //32 agar string first nahi to pehle jo hai wo consider karte hain
+
+
+// console.log(true) //true
+// console.log(+true) //1
+// console.log(true+) //error
+// console.log(+"") //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// ++gameCounter  //101 prefix
+// gameCounter++  //101 postfix
+// console.log(gameCounter); 
